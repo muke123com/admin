8944caf2b728378ed6c66bd5593e02bab70be4a2
muke123com <muke123com@163.com>
2019-08-15 20:17:08 +0800

    init

---
+++ web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <header-box></header-box>
+        <router-view/>
+        <keep-alive>
+            <footer-box></footer-box>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+    import headerBox from './components/header/Header'
+    import footerBox from './components/footer/Footer'
+
+    export default {
+        components: {
+            headerBox,
+            footerBox
+        },
+        name: 'App'
+    }
+</script>
+<style>
+    @import "common.less";
+</style>
